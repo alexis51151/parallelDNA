aaf937906975552b7e6b91ccc99e8a3e4409e83c
Alexis Le Glaunec <leglaun@3a401-13.int-evry.fr>
2020-11-01 14:40:49 +0100

    Deleting apmOMPMPI src and on Makefile

--- apm/mpi_speedup.sh
+++ apm/mpi_speedup.sh
@@ -1,12 +1,12 @@
-BIN="./apmMPI"                               # Name of the binary file
-PATTERNS="AAT TGA TTTTT 		"      # List of patters to test
+BIN="./apmParallel"                               # Name of the binary file
+PATTERNS="AAT TGA TTTTT"      # List of patters to test
 FILE="dna/1mo_chrY.fa"                           # DNA test-file
 APPROXIMATION=0                              # Approximation number
 
-t_seq=$(mpirun -np 1 -f hosts $BIN $APPROXIMATION $FILE $(echo $PATTERNS) | grep APM | awk '{print $5}')
-export OMP_NB_THREADS=1
+t_seq=$(mpirun -np 1 -F hosts $BIN $APPROXIMATION $FILE $(echo $PATTERNS) | grep APM | awk '{print $4}')
+export OMP_NB_THREADS=6
 for i in $(seq 1 12); do
-	t_parallel=$(mpirun -np $i -f hosts $BIN $APPROXIMATION $FILE $(echo $PATTERNS) | grep APM | awk '{print $5}')
+	t_parallel=$(mpirun -np $i -F hosts $BIN $APPROXIMATION $FILE $(echo $PATTERNS) | grep APM | awk '{print $4}')
 	#t_parallel=$(mpirun -np $i $BIN $APPROXIMATION $FILE $(echo $PATTERNS) | grep APM | awk '{print $5}')
         echo -e "$i\t$t_seq\t$t_parallel"
 done
--- apm/run.sh
+++ apm/run.sh
@@ -1,19 +1,18 @@
-export GOMP_CPU_AFFINITY=`seq -s"," 0 48`
+export GOMP_CPU_AFFINITY=`seq -s"," 0 23`
+export OMP_NESTED=true
 
-
-BIN="apmOMP"         	                        # Name of the binary file
+BIN="apmParallel"         	                        # Name of the binary file
 PATTERNS="ATCG TTC AAA AT"       		# List of patters to test
 FILE="dna/chr2.fa"        	  		# DNA test-file
 APPROXIMATION=0                 		# Approximation number
-MPI_NP=1
-
+MPI_NP=2
 
 export OMP_NUM_THREADS=1
-t_seq=$(./apm $APPROXIMATION $FILE $(echo $PATTERNS) | grep APM | awk '{print $4}')
+t_seq=$(mpirun -np $MPI_NP ./apmParallel $APPROXIMATION $FILE $(echo $PATTERNS) | grep APM | awk '{print $4}')
 
-for i in $(seq 1 48); do
+for i in $(seq 1 24); do
 	export OMP_NUM_THREADS=$i
-	t_parallel=$(./$BIN $APPROXIMATION $FILE $(echo $PATTERNS) | grep APM | awk '{print $4}')
+	t_parallel=$(mpirun -np $MPI_NP ./$BIN $APPROXIMATION $FILE $(echo $PATTERNS) | grep APM | awk '{print $4}')
         echo -e "$i\t$t_seq\t$t_parallel"
 done
 
